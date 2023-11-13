1a901d9d7a4b46311b55ff2149baeba8a8aee313
weemizo <mrceleius@gmail.com>
2023-11-13 21:25:37 +0100

    fix(App): fixed the bug with App being declared but never referenced

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
-import Calculator from "./components/Calculator/Calculator.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>,
 );
