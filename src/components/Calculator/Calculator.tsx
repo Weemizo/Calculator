f058c2bff9d70be20816381e0906a1a6992e8ef7
weemizo <mrceleius@gmail.com>
2023-11-24 11:34:17 +0100

    refactor(Calculator): Made it more dynamic, since without it - it looked disgusting,

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,5 +1,5 @@
 import "./Calculator.scss";
-
+import { buttonList } from "../../data";
 export default function Calculator() {
   return (
     <div className="main">
@@ -8,28 +8,16 @@ export default function Calculator() {
       <div className="result"></div>
 
       <div className="input">
-        <button className="num">7</button>
-        <button className="num">8</button>
-        <button className="num">9</button>
-        <button className="del">DEL</button>
-        <p className="break"></p>
-        <button className="num">4</button>
-        <button className="num">5</button>
-        <button className="num">6</button>
-        <button className="num">+</button>
-        <p className="break"></p>
-        <button className="num">1</button>
-        <button className="num">2</button>
-        <button className="num">3</button>
-        <button className="num">-</button>
-        <p className="break"></p>
-        <button className="num">.</button>
-        <button className="num">0</button>
-        <button className="num">/</button>
-        <button className="num">x</button>
-        <p className="break"></p>
-        <button className="reset">RESET</button>
-        <button className="enter">=</button>
+        {
+          buttonList.map((button, id) => {
+            if ((id + 1) % 4 === 0) {
+              return <> <button className={button.class} key={id}>{button.text}</button>
+               <p className="break"></p> </>
+            } else {
+              return <button className={button.class} key={id}>{button.text}</button>
+            }
+          })
+        }
       </div>
     </div>
   );
