5a702801d2f9f4ec385e67b923fe851d6d4db975
Dawid Andrzejewski <mrceleius@gmail.com>
2023-12-24 12:47:55 +0100

    refactor(Data): changed the way of how the data is stored and read

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,23 +1,43 @@
 import "./Calculator.scss";
+// import { useState } from "react";
 import { buttonList } from "../../data";
 export default function Calculator() {
+
+  // const [result, setResult] = useState(0);
+
+  // const num = () => {
+  //   return buttonList[0].text
+
+  // }
+
   return (
     <div className="main">
       <h1>calc</h1>
 
-      <div className="result"></div>
+      <div className="result">
+        {/* <p>{num()}</p> */}
+      </div>
 
       <div className="input">
-        {
-          buttonList.map((button, id) => {
-            if ((id + 1) % 4 === 0) {
-              return <> <button className={button.class} key={id}>{button.text}</button>
-               <p className="break"></p> </>
-            } else {
-              return <button className={button.class} key={id}>{button.text}</button>
-            }
-          })
-        }
+        {buttonList.map((button, id) => {
+          if ((id + 1) % 4 === 0) {
+            return (
+              <>
+                {" "}
+                <button className={button.class} key={id}>
+                  {button.text}
+                </button>
+                <p className="break"></p>{" "}
+              </>
+            );
+          } else {
+            return (
+              <button className={button.class} key={id}>
+                {button.text}
+              </button>
+            );
+          }
+        })}
       </div>
     </div>
   );
--- src/data.ts
+++ src/data.ts
@@ -1,75 +1,92 @@
 export const buttonList = [
-    {
-        id: 1,
-        class: 'num',
-        text: '7'
-    },    {
-        id: 2,
-        class: 'num',
-        text: '8'
-    },    {
-        id: 3,
-        class: 'num',
-        text: '9'
-    },    {
-        id: 4,
-        class: 'del',
-        text: 'DEL'
-    },    {
-        id: 5,
-        class: 'num',
-        text: '4'
-    },    {
-        id: 6,
-        class: 'num',
-        text: '5'
-    },    {
-        id: 7,
-        class: 'num',
-        text: '6'
-    },    {
-        id: 8,
-        class: 'num',
-        text: '+'
-    },    {
-        id: 9,
-        class: 'num',
-        text: '1'
-    },    {
-        id: 10,
-        class: 'num',
-        text: '2'
-    },    {
-        id: 11,
-        class: 'num',
-        text: '3'
-    },    {
-        id: 12,
-        class: 'num',
-        text: '-'
-    },    {
-        id: 13,
-        class: 'num',
-        text: '.'
-    },    {
-        id: 14,
-        class: 'num',
-        text: '0'
-    },    {
-        id: 15,
-        class: 'num',
-        text: '/'
-    },    {
-        id: 16,
-        class: 'num',
-        text: 'x'
-    },    {
-        id: 17,
-        class: 'reset',
-        text: 'RESET'
-    },  {
-        id: 18,
-        class: 'enter',
-        text: '='
-    },
-]
+  {
+    id: 1,
+    class: "num",
+    text: "7",
+  },
+  {
+    id: 2,
+    class: "num",
+    text: "8",
+  },
+  {
+    id: 3,
+    class: "num",
+    text: "9",
+  },
+  {
+    id: 4,
+    class: "del",
+    text: "DEL",
+  },
+  {
+    id: 5,
+    class: "num",
+    text: "4",
+  },
+  {
+    id: 6,
+    class: "num",
+    text: "5",
+  },
+  {
+    id: 7,
+    class: "num",
+    text: "6",
+  },
+  {
+    id: 8,
+    class: "num",
+    text: "+",
+  },
+  {
+    id: 9,
+    class: "num",
+    text: "1",
+  },
+  {
+    id: 10,
+    class: "num",
+    text: "2",
+  },
+  {
+    id: 11,
+    class: "num",
+    text: "3",
+  },
+  {
+    id: 12,
+    class: "num",
+    text: "-",
+  },
+  {
+    id: 13,
+    class: "num",
+    text: ".",
+  },
+  {
+    id: 14,
+    class: "num",
+    text: "0",
+  },
+  {
+    id: 15,
+    class: "num",
+    text: "/",
+  },
+  {
+    id: 16,
+    class: "num",
+    text: "x",
+  },
+  {
+    id: 17,
+    class: "reset",
+    text: "RESET",
+  },
+  {
+    id: 18,
+    class: "enter",
+    text: "=",
+  },
+];
